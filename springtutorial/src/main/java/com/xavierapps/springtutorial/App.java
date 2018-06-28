3d4910ec901e3be1523fcd830060d6c4a62ff1ce
XavierYu <landover.yu@gmail.com>
2018-06-28 23:04:22 +0800

    First Commit
    Copy all files

---
+++ springtutorial/src/main/java/com/xavierapps/springtutorial/App.java
@@ -0,0 +1,13 @@
+package com.xavierapps.springtutorial;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
